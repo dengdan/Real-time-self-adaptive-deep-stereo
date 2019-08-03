ad72c7e1b8a75d77982ed44f6cb3e6ddfa2604a1
dengdan <dengdan@fabu.ai>
2019-08-03 17:32:14 +0800

    add scripts

---
+++ scripts/train.sh
@@ -0,0 +1,7 @@
+python Train.py \
+  --trainingSet datasets/flyingthings3d_monkaa_scene_flow.csv\
+  --validationSet datasets/Stereo2015.csv\
+  --output /data/temp/models/MadNet\
+  --lossWeights 1 1 1 1 1 1 1\
+  --batchSize 1
+  
